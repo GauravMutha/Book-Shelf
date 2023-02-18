a26aa370782f309645c12606cc1cf34bd3d5fc25
Gaurav Mutha <gauravmutha1412@gmail.com>
2023-02-18 22:50:38 +0530

    Adds express router and makes index file in routes directory

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app=express();
 const port =3000;
 
+//uses express router
+app.use('/',require('./routes/index.js'));
+
 app.listen(port,function(err){
     if(err) console.log(`Error in running the server: ${err}`);
 
