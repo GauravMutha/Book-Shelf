5c5a23e973cbde9d66f875128e9ab41191ac9281
Gaurav Mutha <gauravmutha1412@gmail.com>
2023-02-20 19:53:32 +0530

    Controller directory gets its first function to export in home_controller file  and route's index uses  it as call back

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const router=express.Router();
+const homeController=require("../controller/home_controller");
 
 console.log('Router has been loaded');
+router.get('/',homeController.home);
 
 module.exports = router;
