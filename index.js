36b9f38892c40b61d7d6e725443114a03e96c686
Gaurav Mutha <gauravmutha1412@gmail.com>
2023-02-22 21:51:57 +0530

    Installs ejs and sets up view engine and views

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app=express();
 const port =3000;
-
+const path=require('path');
 //uses express router
 app.use('/',require('./routes/index.js'));
-
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname,'views'))
 app.listen(port,function(err){
     if(err) console.log(`Error in running the server: ${err}`);
 
