fc783ec3918ab1cc50a610a25249b853c685ec72
Gaurav Mutha <gauravmutha1412@gmail.com>
2023-02-25 20:04:24 +0530

    Adds a file in models that exports schema for user authentication credentials

---
+++ models/userAuth.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userAuthSchema = new mongoose.Schema({
+    email: { type: String, required: true , unique:true},
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    numberOfPages: { type: st, required: true },
+},{
+    timestamps:true
+})
+
+module.exports.userAuthSchema;
